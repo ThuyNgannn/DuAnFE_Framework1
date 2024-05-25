15de6e9f7140e391c3ad3db13f7fc86e8accf1cf
canh1703 <mcanh3690@gmail.com>
2024-05-25 20:56:50 +0700

    update anh

--- src/app/product-detail/product-detail.component.ts
+++ src/app/product-detail/product-detail.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+
 @Component({
   selector: 'app-product-detail',
   templateUrl: './product-detail.component.html',
   styleUrls: ['./product-detail.component.css']
 })
 export class ProductDetailComponent implements OnInit {
-  
+ 
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit(): void {
+   
     
   }
+
 
 }
